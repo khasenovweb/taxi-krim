9614277fda69bb309b180ffdad274f66b6120070
khasenovweb <hasenovkirill93@gmail.com>
2020-03-23 12:08:31 +0500

    sdjhsdjh

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,16 @@
- 
+ $(document).ready(function(){
+ 	$('input[data-input="date"]').datepicker({
+        timepicker: true,
+        dateFormat: 'd MM yyyy'
+    });
+
+    $('input[data-input="date"]').on('change', function(){
+    	console.log($(this).val());
+    });
+
+    $('select.dropdown').dropdown({
+    	onChange: function(value, text, $selectedItem) {
+    	      console.log(value);
+    	    }
+    });
+ });
